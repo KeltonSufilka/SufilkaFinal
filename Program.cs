2687bbdc2fc2788f7232841f8dd4de633b21d46f
KeltonSufilka <121043368+KeltonSufilka@users.noreply.github.com>
2022-12-19 10:26:30 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//*Program Name: Bank account 
+//Programmer: Sufilka Kelton
+ // Date: 12 / 19 / 2022
+ //Description: Program keeps track of funds and shows your money 
+ 
+
+
+
+
+using Classes;
+using System.Globalization;
+
+var account = new BankAccount("<Soup>", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} balance.");
+
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine(account.Balance);
+account.MakeDeposit(100, DateTime.Now, "friend paid me back");
+Console.WriteLine(account.Balance);
+
+Console.WriteLine(account.GetAccountHistory());
+
+
+
